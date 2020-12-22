28917708687d6856c7c4a633114598dcb43c73ef
Hamou Maamar Sabah <saabaaah@gmail.com>
2020-12-22 22:47:35 +0100

    [master]: + 3. jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  // methode render :
+  render(){
+
+    /// données 
+    let data = "prostam";
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Saying <code>"Hello World"</code> differently!
+          </p>
+          <a
+            className="App-link"
+            href="https://prostam.fr"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By Prostam :)
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
