498ca50b3e0821f689cfcf4775850c009855785a
Hamou Maamar Sabah <saabaaah@gmail.com>
2021-01-11 22:53:11 +0100

    + ajout formulaire contact -> changer champs

--- src/components/contact/AddContact.js
+++ src/components/contact/AddContact.js
@@ -11,20 +11,20 @@ class AddContact extends Component {
         const {nom, email, phone} = this.state;
         return (
             <div>
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Ajouter Contact</h4>
-                        <div class="form-group">
+                <div className="card">
+                    <div className="card-body">
+                        <h4 className="card-title">Ajouter Contact</h4>
+                        <div className="form-group">
                           <label for="">Nom</label>
-                          <input type="text" name="nom" id="nom"   value={nom} class="form-control" placeholder="" aria-describedby="helpId"/>
+                          <input type="text" name="nom" id="nom"   value={nom} className="form-control" placeholder="" aria-describedby="helpId"/>
                         </div>
-                        <div class="form-group">
+                        <div className="form-group">
                           <label for="">Email</label>
-                          <input type="text" name="email" id="email" value={email} class="form-control" placeholder="" aria-describedby="helpId"/>
+                          <input type="text" name="email" id="email" value={email} className="form-control" placeholder="" aria-describedby="helpId"/>
                         </div>
-                        <div class="form-group">
+                        <div className="form-group">
                           <label for="">Phone</label>
-                          <input type="text" name="phone" id="phone" value={phone} class="form-control" placeholder="" aria-describedby="helpId"/>
+                          <input type="text" name="phone" id="phone" value={phone} className="form-control" placeholder="" aria-describedby="helpId"/>
                         </div>
                     </div>
                 </div>
